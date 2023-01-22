32249b671c873d8c4524aca41024688c86394ce8
rivesunder <rivesunder@protonmail.com>
2023-01-21 18:11:09 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+#  packages=["sepia", "testing"], \
+
+setup(\
+        name="JAKInhibition", \
+        version = "0.00", \
+        install_requires =[\
+                "coverage==6.4.3",\
+                "jax>=0.3.15",\
+                "jaxlib>=0.3.15",\
+                "numpy==1.23.1",\
+                ],\
+        packages=find_packages(),\
+        description = "estimating inhibition metrics of small molecules for JAK kinases"
+    )
+
+
